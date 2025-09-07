9d53b34c77fa29e36c027425a297499962c63aeb
tylinndd <tylinjd1@gmail.com>
2025-09-07 00:40:05 -0400

    close

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-#read the csv file
+
 df = pd.read_csv("/Users/tylin/movie-recommendation/movie_dataset.csv")
-print(df.columns)
+
 
 def get_title_from_index(index):
     return df[df.index == index]["title"].values[0]
@@ -13,24 +13,33 @@ def get_title_from_index(index):
 def get_index_from_title(title):
     return df[df.title == title]["index"].values[0]
 
-##Step 2: Select Features
-features = ["keywords","case","genres","director"]
+
+features = ["keywords","cast","genres","director"]
 for feature in features:
     df[feature] = df[feature].fillna('')
-##Step 3: Create a column in DF which combines all selected features
-all_features = df["keywords"] + ' ' + df["case"] + ' ' + df["genres"] + ' ' + df["director"]
-df["all_features"] = all_features
-##Step 4: Create count matrix from this new combined column
-count_matrix = CountVectorizer().fit_transform(df["all_features"])
-##Step 5: Compute the Cosine Similarity based on the count_matrix
+def combine_features(row):
+    try:
+        return row['keywords'] + ' ' + row['cast'] + ' ' + row['genres'] + ' ' + row['director']
+    except:
+        print("Error:", row)
+
+df["combined_features"] = df.apply(combine_features, axis=1)
+print(df["combined_features"].head())      
+
+
+
+count_matrix = CountVectorizer().fit_transform(df["combined_features"])
+
 cosine_similarity = cosine_similarity(count_matrix)
 movie_user_likes = "Avatar"
-
-## Step 6: Get index of this movie from its title
-
-## Step 7: Get a list of similar movies in descending order of similarity score
-
-
-## Step 8: Print titles of first 50 movies
+movie = get_index_from_title(movie_user_likes)
+similar_movies = list(enumerate(cosine_similarity[movie]))
+similar_movies_sorted = sorted(similar_movies, key= lambda x:x[1],reverse = True)
+i=0
+for movie in similar_movies_sorted:
+    print(get_title_from_index(movie[0]))
+    i=i+1
+    if i>50:
+        break
 
  
